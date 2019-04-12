27c79ee0b131c13cf0991250bbef127917abada5
eipc16 <pprzemek.312@gmail.com>
2019-04-12 11:42:58 +0200

    renamed FutoshikiVariable to PuzzleVariable

--- src/pp/pwr/variables/PuzzleVariable.java
+++ src/pp/pwr/variables/PuzzleVariable.java
@@ -2,20 +2,20 @@ package pp.pwr.variables;
 
 import pp.pwr.domains.Domain;
 
-public class FutoshikiVariable extends Variable<Integer> implements Comparable<FutoshikiVariable> {
+public class PuzzleVariable extends Variable<Integer> implements Comparable<PuzzleVariable> {
 
     private int row_position, column_position;
 
     private static final String[] column_labels = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"};
     private static final String[] row_labels = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
 
-    public FutoshikiVariable(Integer value, int column_position, int row_position, Domain domain, boolean predefined) {
+    public PuzzleVariable(Integer value, int column_position, int row_position, Domain domain, boolean predefined) {
         super(value, domain, predefined);
         this.row_position = row_position;
         this.column_position = column_position;
     }
 
-    public FutoshikiVariable(Integer value, int column_position, int row_position, Domain domain) {
+    public PuzzleVariable(Integer value, int column_position, int row_position, Domain domain) {
         this(value, row_position, column_position, domain, false);
     }
 
@@ -25,7 +25,7 @@ public class FutoshikiVariable extends Variable<Integer> implements Comparable<F
     }
 
     @Override
-    public int compareTo(FutoshikiVariable futoshikiVariable) {
-        return this.value.compareTo(futoshikiVariable.value);
+    public int compareTo(PuzzleVariable puzzleVariable) {
+        return this.value.compareTo(puzzleVariable.value);
     }
 }
