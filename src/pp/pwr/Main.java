7a74626d3345d8c8908253c928fce430d1420afe
eipc16 <pprzemek.312@gmail.com>
2019-04-12 13:20:39 +0200

    completed file loading

--- src/pp/pwr/Main.java
+++ src/pp/pwr/Main.java
@@ -1,8 +1,12 @@
 package pp.pwr;
 
+import pp.pwr.models.FutoshikiModel;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String path = "src/pp/pwr/Data/Research/test_futo_4_0.txt";
+
+	    FutoshikiModel futoshikiModel = new FutoshikiModel(path);
     }
 }
